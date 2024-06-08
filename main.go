3cecbf966119263dfbd08274a75048f89f4e9e4a
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-06-08 13:41:30 -0300

    feat(crud): adds crud

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type Movie struct {
@@ -20,16 +22,73 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
+var movies []Movie
+
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(movies)
 }
-func getMovie(w http.ResponseWriter, r *http.Request)    {}
-func createMovie(w http.ResponseWriter, r *http.Request) {}
-func updateMovie(w http.ResponseWriter, r *http.Request) {}
-func deleteMovie(j http.ResponseWriter, r *http.Request) {}
 
-var movies []Movie
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	movie.ID = strconv.Itoa(rand.Intn(100000))
+	movies = append(movies, movie)
+
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+
+			movie.ID = strconv.Itoa(rand.Intn(100000))
+			movies = append(movies, movie)
+
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
+}
 
 func main() {
 	router := mux.NewRouter()
